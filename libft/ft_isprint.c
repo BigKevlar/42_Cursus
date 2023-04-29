93e3ac3572499cc8108b425f2e832b03cff0365b
Juan Carlos Martos Vergara <jmartos-@c1r13s2.42malaga.com>
2023-04-29 15:18:10 +0200

    Fixing errors and adding new functions

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/23 20:14:49 by kevlar            #+#    #+#             */
-/*   Updated: 2023/04/29 15:13:35 by jmartos-         ###   ########.fr       */
+/*   Created: 2023/04/29 15:00:08 by jmartos-          #+#    #+#             */
+/*   Updated: 2023/04/29 15:16:40 by jmartos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<ctype.h> /*LIBRERIA DE TODAS LAS FUNCIONES TIPO IS.*/
 #include	<stdio.h> /*LIBRERIA PARA EL PRINTF DEL MAIN.*/
 
-/*ESTA FUNCION COMPRUEBA SI UN CARACTER ES UN CARACTER ASCII.*/
+/*ESTA FUNCION COMPRUEBA SI UN CARACTER ES IMPRIMIBLE (VER TABLA ASCII).*/
 int	ft_isascii(int x)
 {
-	if ((x >= 0 && x <= 127))
+	if ((x >= 32 && x <= 126))
 		return (1);
 	return (0);
 }
@@ -25,8 +25,8 @@ int	main(void)
 {
 	char	x;
 
-	x = '+';
-	printf("%d",ft_isascii(x));
+	x = '@';
+	printf("%d",ft_isalnum(x));
 	return (0);
 }
 */
