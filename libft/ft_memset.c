5d3e35770c495cab1be323c8286d8d471b914594
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-05-01 18:14:42 +0200

    Solving problems

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -3,41 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmartos- <jmartos-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 16:25:03 by jmartos-          #+#    #+#             */
-/*   Updated: 2023/05/01 15:22:40 by jmartos-         ###   ########.fr       */
+/*   Updated: 2023/05/01 18:13:55 by jmartos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h" /*LIBRERIA CUSTOM*/
 
-/*ESTA FUNCION COPIA EN "STR" EL CARACTER "C" EN LAS PRIMERAS "N" POSICIONES.*/
-void	*memset(void *b, int c, size_t len);
+/*ESTA FUNCION COPIA EN "S" EL CARACTER "C" EN LAS PRIMERAS "N" POSICIONES.*/
+void	*memset(void *s, int c, size_t n);
 {
-	unsigned char	*str;
-	int				i;
-
-	str = (unsigned char *)s;
-	i = 0;
-	if (n == 0)
-		return ;
-	while (i < n)
-	{
-		str[i] = 0;
-		i++;
-	}
-	return ;
+	
 }
-
-/*
-int main ()
-{
-	unsigned char	*phrase;
-
-	phrase = "FundacionTelefonica";
-	printf(phrase);
-	printf(ft_bzero(phrase, 7));
-	return (0);
-}
-*/
