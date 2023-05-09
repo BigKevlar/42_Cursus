d78d20c9d19675e171473b954fc41213cd06802c
Juan Carlos Martos Vergara <jmartos-@c1r10s3.42malaga.com>
2023-05-09 14:15:39 +0200

    memmove done

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,28 +6,39 @@
 /*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 15:35:50 by jmartos-          #+#    #+#             */
-/*   Updated: 2023/05/06 15:53:09 by jmartos-         ###   ########.fr       */
+/*   Updated: 2023/05/09 14:14:29 by jmartos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	"libft.h" /*LIBRERIA CUSTOM*/
+#include    "libft.h" /*LIBRERIA CUSTOM*/
 
-/*ESTA FUNCION COPIA EN "SRC" LOS "N" DIGITOS EN "DST".*/
+/*ESTA FUNCION COPIA "len" BYTES DE "src" EN "dst".*/
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*from;
-	unsigned char	*to;
+	char	*src1;
+	char	*dst1;
+	int		cont;
 
-	from = (unsigned char *)src;
-	to = (unsigned char *)dst;
-	while (len)
+	src1 = (char *)src;
+	dst1 = (char *)dst;
+	cont = 0;
+	while (len > 0)
 	{
-		*to = *from;
-		to++;
-		from++;
+		dst1[cont] = src1[cont];
+		cont++;
 		len--;
 	}
-	return (dst);
+	return (dst1);
 }
-/*EL "if" HACE QUE SI SON NULOS ALGUNO DE LOS DOS RETORNA 0.*/
-/*LA PARTE DE "return (dst)" PERMITE DEVOLVER EL PUNTERO DESDE EL PRINCIPIO.*/
+/*EL "if" HACE QUE SI SON NULOS ALGUNO DE LOS DOS DEVUELVE 0.*/
+/*LA PARTE DE "return (dst)" PERMITE DEVOLVER EL PUNTERO/ARRAY AL PRINCIPIO.*/
+/*
+int main(void)
+{
+    char dst[] = "Caracola";
+    char src[] = "Hola";
+
+    printf("%s", ft_memmove(dst, src, 3));
+    return (0);
+}
+*/
