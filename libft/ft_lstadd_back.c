0bd4f53e3460f89c123e3c6df344a5ced611718d
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-06-04 20:37:33 +0200

    Checked with descriptions and main commented

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -3,29 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmartos- <jmartos-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 13:13:18 by jmartos-          #+#    #+#             */
-/*   Updated: 2023/06/03 14:37:35 by jmartos-         ###   ########.fr       */
+/*   Updated: 2023/06/04 19:17:28 by jmartos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h" /*LIBRERIA CUSTOM "jmartos-"*/
 
 /*#Añade el nodo "new" al final de la lista "lst". Nos valemos de la
-funcion "ft_ltslast" para meter en la nueva lista "last" la ultima posicion.#*/
+funcion "ft_ltslast" para meter en la nueva lista "last" la ultima posicion.
+La parte de "last->next = new" Hara que el puntero de "next" aunte a "new".#*/
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (lst)
+	if (!*lst)
+		*lst = new;
+	else
 	{
-		if (*lst)
-		{
-			last = ft_lstlast(*lst);
-			last->next = new;
-		}
-		else
-			*lst = new;
+		last = ft_lstlast(*lst);
+		last->next = new;
 	}
 }
--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -3,24 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmartos- <jmartos-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 12:57:43 by jmartos-          #+#    #+#             */
-/*   Updated: 2023/06/03 14:33:41 by jmartos-         ###   ########.fr       */
+/*   Updated: 2023/06/04 16:30:30 by jmartos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h" /*LIBRERIA CUSTOM "jmartos-"*/
 
-/*#Devuelve el último nodo de la lista. La parte de "lst = lst->next" va
-avanzando hasta toparse con el ultimo nodo.#*/
+/*#Localiza el puntero al último nodo de la lista. La parte de
+"lst = lst->next" va machacandose con el puntero next del siguiente en
+la lista y vuelve a comprobarse de nuevo, hasta toparse con el ultimo nodo.#*/
 t_list	*ft_lstlast(t_list *lst)
 {
-	while (lst)
-	{
-		if (!lst->next)
-			return (lst);
+	if (!lst)
+		return (NULL);
+	while (lst->next)
 		lst = lst->next;
-	}
 	return (lst);
 }
