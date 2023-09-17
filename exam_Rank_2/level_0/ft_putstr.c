76c586dac0e2f218ab6c4b0559a949b9365125f0
Juan Carlos Martos Vergara <jmartos-@c3r7s1.42malaga.com>
2023-09-17 18:17:20 +0200

    ft_putstr

---
+++ exam_Rank_2/level_0/ft_putstr.c
@@ -0,0 +1,29 @@
+/*
+Assignment name  : ft_putstr
+Expected files   : ft_putstr.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a function that displays a string on the standard output.
+
+The pointer passed to the function contains the address of the string's first
+character.
+
+Your function must be declared as follows:
+
+void	ft_putstr(char *str);
+*/
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int cont = 0;
+
+	while(str[cont] != '\0')
+	{
+		write(1, &str[cont], 1);
+		cont++;
+	}
+	return ;
+}
