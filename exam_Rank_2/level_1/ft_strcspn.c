2d5bdac3612bdfc010fb6272185c3c1eef99c609
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-09-25 17:17:30 +0200

    ft_strcspn

---
+++ exam_Rank_2/level_1/ft_strcspn.c
@@ -0,0 +1,34 @@
+/*
+Assignment name	: ft_strcspn
+Expected files	: ft_strcspn.c
+Allowed functions: None
+---------------------------------------------------------------
+
+Reproduce exactly the behavior of the function strcspn
+(man strcspn).
+
+The function should be prototyped as follows:
+
+size_t	ft_strcspn(const char *s, const char *reject);
+*/
+
+#include <string.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	size_t c1 = 0;
+	size_t c2 = 0;
+
+	while (s[c1] != '\0')
+	{
+		while (reject[c2] != '\0')
+		{
+			if (reject[c2] == s[c1])
+				return (c1);
+			c2++;
+		}
+		c2 = 0;
+		c1++;
+	}
+	return (c1);
+}
