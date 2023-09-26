8e615db2ccb655fd2b0bf3036aad4d5b11f8d98c
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-09-26 19:10:35 +0200

    hidenp

---
+++ exam_Rank_2/level_2/hidenp.c
@@ -0,0 +1,52 @@
+/*
+Assignment name  : hidenp
+Expected files   : hidenp.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program named hidenp that takes two strings and displays 1
+followed by a newline if the first string is hidden in the second one,
+otherwise displays 0 followed by a newline.
+
+Let s1 and s2 be strings. We say that s1 is hidden in s2 if it's possible to
+find each character from s1 in s2, in the same order as they appear in s1.
+Also, the empty string is hidden in any string.
+
+If the number of parameters is not 2, the program displays a newline.
+
+Examples :
+
+$>./hidenp "fgex.;" "tyf34gdgf;'ektufjhgdgex.;.;rtjynur6" | cat -e
+1$
+$>./hidenp "abc" "2altrb53c.sse" | cat -e
+1$
+$>./hidenp "abc" "btarc" | cat -e
+0$
+$>./hidenp | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int	cont1 = 0;
+	int	cont2 = 0;
+
+	if (ac == 3)
+	{
+		while (av[1][cont1] != '\0' && av[2][cont2] != '\0')
+		{
+			if (av[1][cont1] == av[2][cont2])
+				cont1++;
+			cont2++;
+		}
+		if (av[1][cont1] == '\0')
+			write(1, "1", 1);
+		else if (av[2][cont2] == '\0')
+			write(1, "0", 1);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
