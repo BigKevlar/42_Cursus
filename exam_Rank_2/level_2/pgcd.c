82421b1ed624f1024c01040b346111a5fb16c28f
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-09-27 18:30:38 +0200

    pgcd

---
+++ exam_Rank_2/level_2/pgcd.c
@@ -0,0 +1,57 @@
+/*
+Assignment name  : pgcd
+Expected files   : pgcd.c
+Allowed functions: printf, atoi, malloc, free
+--------------------------------------------------------------------------------
+
+Write a program that takes two strings representing two strictly positive
+integers that fit in an int.
+
+Display their highest common denominator followed by a newline (It's always a
+strictly positive integer).
+
+If the number of parameters is not 2, display a newline.
+
+Examples:
+
+$> ./pgcd 42 10 | cat -e
+2$
+$> ./pgcd 42 12 | cat -e
+6$
+$> ./pgcd 14 77 | cat -e
+7$
+$> ./pgcd 17 3 | cat -e 
+1$
+$> ./pgcd | cat -e
+$
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void maxcd(unsigned int n1, unsigned int n2)
+{
+	int aux = n1;
+	
+	while (aux > 0)
+	{
+		if ((n1 % aux == 0) && (n2 % aux == 0))
+		{
+			printf("%d", aux);
+			return ;
+		}
+		--aux;
+	}
+}
+
+int main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		unsigned int n1 = atoi(av[1]);
+		unsigned int n2 = atoi(av[2]);
+		maxcd(n1, n2);
+	}
+	printf("\n");
+	return (0);
+}
