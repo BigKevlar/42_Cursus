5e254dcbe6f59bc4ad1aa7ae7cbafce069c87635
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-09-29 10:59:12 +0200

    ft_itoa

--- exam_Rank_2/level_3/ft_itoa.c
+++ exam_Rank_2/level_3/ft_itoa.c
@@ -12,7 +12,40 @@ Your function must be declared as follows:
 char	*ft_itoa(int nbr);
 */
 
-char	*ft_itoa(int nbr)
+#include <stdlib.h>
+
+char *ft_itoa(int nbr) 
 {
+    int n = nbr;
+	int len = 0;
 	
+    if(nbr == -2147483648)
+		return("-2147483648\0");
+	if (nbr <= 0)
+		len++;
+	while (n) 
+	{
+		n /= 10;
+		len++;
+	}
+	char *result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL) 
+		return NULL;
+	result[len] = '\0';
+	if (nbr == 0)
+	{
+		result[0] = '0';
+		return(result);
+	}
+	if (nbr < 0) 
+	{
+		result[0] = '-';
+		nbr = -nbr;
+	}
+	while (nbr) 
+	{
+		result[--len] = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	return result;
 }
