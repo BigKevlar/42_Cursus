3b32863b3b2ca60c75381cee7bd6fad2c805b70a
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-09-29 10:56:28 +0200

    ft_swap

---
+++ exam_Rank_2/level_0/ft_swap.c
@@ -0,0 +1,22 @@
+/*
+Assignment name  : ft_swap
+Expected files   : ft_swap.c
+Allowed functions: 
+--------------------------------------------------------------------------------
+
+Write a function that swaps the contents of two integers the adresses of which
+are passed as parameters.
+
+Your function must be declared as follows:
+
+void	ft_swap(int *a, int *b);
+*/
+
+void	ft_swap(int *a, int *b)
+{
+	int	aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
