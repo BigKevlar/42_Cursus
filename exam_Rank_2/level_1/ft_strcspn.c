d56ae384e4d569a6d865e8f4da942692a3bc2cb8
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-10-01 23:08:56 +0200

    ft_strcspn

--- exam_Rank_2/level_1/ft_strcspn.c
+++ exam_Rank_2/level_1/ft_strcspn.c
@@ -14,20 +14,20 @@ size_t	ft_strcspn(const char *s, const char *reject);
 
 #include <string.h>
 
-size_t	ft_strcspn(const char *s, const char *reject)
+size_t	ft_strcspn(const char *s, const char *reject) //busca la longuitud de un segmento inicial reject en otra s.
 {
 	size_t c1 = 0;
 	size_t c2 = 0;
 
 	while (s[c1] != '\0')
 	{
+		c2 = 0;
 		while (reject[c2] != '\0')
 		{
 			if (reject[c2] == s[c1])
 				return (c1);
 			c2++;
 		}
-		c2 = 0;
 		c1++;
 	}
 	return (c1);
