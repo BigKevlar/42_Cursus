3195865710054430bafe9e1dbb01309cb5f5c93f
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-10-01 23:08:48 +0200

    ft_strcmp

--- exam_Rank_2/level_1/ft_strcmp.c
+++ exam_Rank_2/level_1/ft_strcmp.c
@@ -11,11 +11,11 @@ Your function must be declared as follows:
 int    ft_strcmp(char *s1, char *s2);
 */
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2) //compara un string con otro.
 {
 	int	cont = 0;
 
-	while (s1[cont] != '\0' && s2[cont] != '\0' && s1[cont] == s2[cont])
+	while (s1[cont] != '\0' && s2[cont] != '\0' && s1[cont] == s2[cont]) //mientras ninguno de los dos strings sea nulo y mientras sean las palabras iguales, avanza.
 		cont++;
-	return (s1[cont] - s2[cont]);
+	return (s1[cont] - s2[cont]); //cuando ya son diferentes algunas palabras o termina alguno de ellos o los dos, devuelve la resta entre ellos.
 }
