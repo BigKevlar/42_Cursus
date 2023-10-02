a03dcaca2c51ea91e3e5ffbbc316f913f0c5e3e5
BigKevlar <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>
2023-10-02 15:11:18 +0200

    rev_print

--- exam_Rank_2/level_0/rev_print.c
+++ exam_Rank_2/level_0/rev_print.c
@@ -29,7 +29,7 @@ int	main(int ac, char **av)
 	{
 		while (av[1][cont] != '\0') //avanzamos hasta el final.
 			cont++;
-		while (cont >= 0) // imprimimos en sentido contrario.
+		while (cont >= 0) //imprimimos en sentido contrario.
 		{
 			write(1, &av[1][cont], 1);
 			cont--;
