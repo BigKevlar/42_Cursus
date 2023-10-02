493c172b512631945355399b63a2cf89f108421e
BigKevlar <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>
2023-10-02 15:12:06 +0200

    ft_strlen

---
+++ exam_Rank_2/level_0/ft_strlen.c
@@ -0,0 +1,21 @@
+/*
+Assignment name  : ft_strlen
+Expected files   : ft_strlen.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Write a function that returns the length of a string.
+
+Your function must be declared as follows:
+
+int	ft_strlen(char *str);
+*/
+
+int	ft_strlen(char *str)
+{
+	int cont = 0;
+
+	while (str[cont] != '\0')
+		cont++;
+	return (cont);
+}
