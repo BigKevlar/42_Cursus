3f0cb9071fffc019080d78dc8877a23c35c20981
BigKevlar <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>
2023-10-04 00:33:11 +0200

    print_hex

---
+++ exam_Rank_2/level_2/print_hex.c
@@ -0,0 +1,55 @@
+/*
+Assignment name  : print_hex
+Expected files   : print_hex.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a positive (or zero) number expressed in base 10,
+and displays it in base 16 (lowercase letters) followed by a newline.
+
+If the number of parameters is not 1, the program displays a newline.
+
+Examples:
+
+$> ./print_hex "10" | cat -e
+a$
+$> ./print_hex "255" | cat -e
+ff$
+$> ./print_hex "5156454" | cat -e
+4eae66$
+$> ./print_hex | cat -e
+$
+*/
+
+#include <unistd.h>
+
+int	mini_atoi(char *str)
+{
+	int	c = 0;
+	int	num = 0;
+
+	while (str[c] != '\0')
+	{
+		num = (num * 10) + (*str - '0');
+		str++;
+	}
+	return (num);
+}
+
+void	print_hex(int n)
+{
+	char	hex[16] = "0123456789abcdef";
+	
+	if (n >= 16)
+		print_hex(n / 16);
+	write(1, &hex[n % 16], 1);
+	return ;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		print_hex(mini_atoi(argv[1]));
+	write(1, "\n", 1);
+	return (0);
+}
