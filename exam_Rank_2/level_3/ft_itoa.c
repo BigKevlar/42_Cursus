8dcbec9f7d80acf09140c09b69481a894c9de0b0
BigKevlar <juancarlosmartosvergara@gmail.com>
2023-10-04 16:53:49 +0200

    ft_itoa

--- exam_Rank_2/level_3/ft_itoa.c
+++ exam_Rank_2/level_3/ft_itoa.c
@@ -5,7 +5,7 @@ Allowed functions: malloc
 --------------------------------------------------------------------------------
 
 Write a function that takes an int and converts it to a null-terminated string.
-The function returns the result in a char array that you must allocate.
+The function returns the str in a char array that you must allocate.
 
 Your function must be declared as follows:
 
@@ -13,39 +13,57 @@ char	*ft_itoa(int nbr);
 */
 
 #include <stdlib.h>
+#include <stdio.h>
 
-char *ft_itoa(int nbr) 
+char *ft_itoa(int nbr) //Integer TO Ascii (numero a caracter)
 {
-    int n = nbr;
-	int len = 0;
+	long	tem = nbr;
+	int		sig = 1;
+	int		len = 0;
+	char	*str;
 	
-    if(nbr == -2147483648)
-		return("-2147483648\0");
-	if (nbr <= 0)
+	if(tem < 0) //si el numero es negativo contamos una posicion para contar el simbolo negativo.
+	{
 		len++;
-	while (n) 
+		sig = -1;
+	}
+	while (tem != 0) //calculamos cuantas posiciones tiene el numero, usando tem para saltar cifras a la izquierda y sumando 1 a len.
 	{
-		n /= 10;
+		tem /= 10;
 		len++;
 	}
-	char *result = (char *)malloc(sizeof(char) * (len + 1));
-	if (result == NULL) 
-		return NULL;
-	result[len] = '\0';
-	if (nbr == 0)
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str) 
+		return (NULL);
+	str[len] = '\0'; //ponemos el caracter nulo en la ultima posicion de de str aprovechando que len esta en esa posicion de memoria.
+	if (nbr == 0) //si el numero es cero, ponemos 0 en la primera posicion y lo devolvemos tal cual, ya que la ultima ya es nulo.
 	{
-		result[0] = '0';
-		return(result);
+		str[0] = '0';
+		return(str);
 	}
-	if (nbr < 0) 
+	if (nbr < 0) //si es menos que cero, ponemos un - en la primera posicion y saltamos posicion.
+		str[0] = '-';
+	while (nbr != 0) //mientras no llegue a 0
 	{
-		result[0] = '-';
-		nbr = -nbr;
-	}
-	while (nbr) 
-	{
-		result[--len] = nbr % 10 + '0';
+		len--;
+		str[len] = (nbr % 10) * sig + '0';
 		nbr /= 10;
 	}
-	return result;
+	return (str);
+}
+
+int	main(void)
+{
+	printf("%s\n", ft_itoa(33));
+	printf("%s\n", ft_itoa(-33));
+	printf("%s\n", ft_itoa(12345));
+	printf("%s\n", ft_itoa(-12345));
+	printf("%s\n", ft_itoa(98765));
+	printf("%s\n", ft_itoa(-98765));
+	printf("%s\n", ft_itoa(45));
+	printf("%s\n", ft_itoa(-45));
+	printf("%s\n", ft_itoa(-2147483648));
+	printf("%s\n", ft_itoa(2147483647));
+	printf("%s\n", ft_itoa(0));
+	return (0);
 }
