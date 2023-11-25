f8de6b6ca7212f31e5e3db7036233923726d5cf9
BigKevlar <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>
2023-11-25 11:41:00 +0100

    rev_print

--- exam_Rank_2/level_0/rev_print.c
+++ exam_Rank_2/level_0/rev_print.c
@@ -23,19 +23,31 @@ $
 
 int	main(int ac, char **av)
 {
-	int		cont = 0;
+	int		c = 0;
 
 	if (ac == 2)
 	{
-		while (av[1][cont] != '\0') //avanzamos hasta el final.
-			cont++;
-		cont--; //retrocedemos 1 posicion, porque no queremos imprimir el caracter nulo.
-		while (cont >= 0) //imprimimos en sentido contrario.
+		while (av[1][c] != '\0')
+			c++;
+		c--;
+		while (c >= 0)
 		{
-			write(1, &av[1][cont], 1);
-			cont--;
+			write(1, &av[1][c], 1);
+			c--;
 		}
 	}
 	write(1, "\n", 1);
 	return (0);
 }
+
+/*
+Pseudocódigo:
+--------------------------------------------------------------------------------
+Incluimos la libreria "unistd.h" para usar la funcion "write".
+Nuestro programa imprime un string en sentido contrario.
+Vamos a declarar un contador llamado "c" para iterar entre letras.
+Mientras sean solo dos argumentos realiza lo de dentro.
+Hasta que el string no llegue al final avanza "c", luego retrocede una posicion para evitar imprimir el caracter nulo
+Mientras "c" sea mayor o igual que cero escribe cada letra y resta uno a "c" hasta llegar al principio.
+Ponemos un salto de linea y finalizamos programa.
+*/
