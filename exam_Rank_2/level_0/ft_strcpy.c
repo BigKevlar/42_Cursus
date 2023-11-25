db278427dcbdd743114c6166e425d92e9182c0a3
BigKevlar <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>
2023-11-25 11:39:58 +0100

    ft_strcpy

--- exam_Rank_2/level_0/ft_strcpy.c
+++ exam_Rank_2/level_0/ft_strcpy.c
@@ -13,13 +13,23 @@ char    *ft_strcpy(char *s1, char *s2);
 
 char	*ft_strcpy(char *s1, char *s2)
 {
-	int cont = 0;
+	int c = 0;
 
-	while (s2[cont] != '\0')
+	while (s2[c] != '\0')
 	{
-		s1[cont] = s2[cont];
-		cont++;
+		s1[c] = s2[c];
+		c++;
 	}
-	s1[cont] = '\0';
+	s1[c] = '\0';
 	return (s1);
 }
+
+/*
+Pseudocódigo:
+--------------------------------------------------------------------------------
+Nuestra funcion copia en string "s2" sobre el string "s1".
+Inicamos un contacdor "c" que iterará cada letra del string.
+Mientras que el segundo string no llegue a nulo copia cada letra en la misma posicion del en el primero.
+Por ultimo pone al final el caracter nulo.
+Funaliza la funcion devolviendo el primero con todo copiado.
+*/
