006c3f83de2282096809f3b4c4dcf67536d9a70e
BigKevlar <kevlaroscuro@gmail.com>
2024-01-16 18:50:24 +0100

    rot_13

--- exam_Rank_2/level_0/rot_13.c
+++ exam_Rank_2/level_0/rot_13.c
@@ -37,13 +37,13 @@ int	main(int ac, char **av)
 
 	if (ac == 2)
 	{
-		while (av[1][c] != '\0') //bucle hasta el final del string.
+		while (av[1][c] != '\0')
 		{
-			if ((av[1][c] >= 'a' && av[1][c] <= 'm') || (av[1][c] >= 'A' && av[1][c] <= 'M')) //si la letra está entre las primeras 13 letras del abecedario sumale 13 posiciones.
+			if ((av[1][c] >= 'a' && av[1][c] <= 'm') || (av[1][c] >= 'A' && av[1][c] <= 'M'))
 				av[1][c] += 13;
-			else if ((av[1][c] >= 'n' && av[1][c] <= 'z') || (av[1][c] >= 'N' && av[1][c] <= 'Z')) //si la letra está entre las ultimas 13 letras del abecedario restale 13 posiciones, que es como dar la vuelta.
+			else if ((av[1][c] >= 'n' && av[1][c] <= 'z') || (av[1][c] >= 'N' && av[1][c] <= 'Z'))
 				av[1][c] -= 13;
-			write (1, &av[1][c], 1); //tanto si entra en un if como si no pues escribe.
+			write (1, &av[1][c], 1);
 			c++;
 		}
 	}
@@ -54,9 +54,17 @@ int	main(int ac, char **av)
 /*
 Pseudocódigo:
 --------------------------------------------------------------------------------
-Incluimos la libreria "unistd.h" para usar la funcion "write".
 Nuestro programa imprime un string reemplazando cada letra por la que está 13 posiciones en adelante.
+Incluimos la libreria "unistd.h" para usar la funcion "write".
 Camos a declarar un contador llamado "c" para iterar entre letras.
 Mientras sean solo dos argumentos realiza lo de dentro.
-
+Vamos a ir recorriendo todo el string e imprimiendo lo que nos encontremos pero con dos condiciones:
+si la letra encontrada esta entre la 13 primeras ("a" y "m"), da igual si son mayusculas o minusculas, aumenta 13 posiciones,
+y si la letra esta entre las 13 ultimas ("n" y "z") resta 13 posiciones.
+Luego imprime
+Ejemplo:
+si la letra es la "i" aumentara 13 posiciones hasta la "u", si es una "t" imprimira una "g" porque resta 13 posiciones,
+que es lo mismo que si llegasemos a la "z" y diesemos la vuelta al abecedario.
+Luego imprime. Si no es una lletra no entraria en ninguna condicion e imprimiria directamente.
+Ponemos un salto de linea y finalizamos programa.
 */
