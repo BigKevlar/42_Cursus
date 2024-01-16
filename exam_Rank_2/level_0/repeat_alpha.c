7c42e3256b036f1287764ecbf6067f5615221d9f
BigKevlar <kevlaroscuro@gmail.com>
2024-01-16 18:49:54 +0100

    repeat_alpha

--- exam_Rank_2/level_0/repeat_alpha.c
+++ exam_Rank_2/level_0/repeat_alpha.c
@@ -43,9 +43,9 @@ int	main(int ac, char **av)
 		{
 			word = 1;
 			if (av[1][c] >= 'A' && av[1][c] <= 'Z')
-				word = word + (av[1][c] - 'A');
+				word += (av[1][c] - 'A');
 			if (av[1][c] >= 'a' && av[1][c] <= 'z')
-				word = word + (av[1][c] - 'a');
+				word += (av[1][c] - 'a');
 			while (word > 0)
 			{
 				write(1, &av[1][c], 1);
@@ -61,13 +61,15 @@ int	main(int ac, char **av)
 /*
 Pseudocódigo:
 --------------------------------------------------------------------------------
-Nuestro programa imprimirá por pantalla cada letra del string tantas veces como sea su posicion en el avecedario.
+Nuestro programa imprimirá por pantalla cada letra del string tantas veces como sea su posicion en el abecedario.
 Incluimos la libreria "unistd.h" para usar la funcion "write".
-Vamos a declarar dos variables: un contador llamado "c" para iterar entre letras, y "word" simepre en 1 para er el valor numerico del abecedario.
+Vamos a declarar dos variables: un contador llamado "c" para iterar entre letras, y "word" simepre en 1 para el valor numerico del abecedario.
 Si el progama recibe exactamente dos argumentos realiza el bucle, si no solo imprime un salto de linea y finaliza el rpograma.
 Mientras el argumento string no sea nulo, con "word" comenzando siempre en 1 para que minimo imprima una vez:
-resta la letra entre "A" o "a" para obtener su posicion en el abecedario.
-Luego imprime cada letra tantas veces como su posicion en el abecedario, que se a guardado en "word".
-Pasamos a la siguiente letra aumentando "c", y asi con todo el string.
+se resta la letra entre "A" o "a" para obtener su posicion en el abecedario.
+Por ejemplo:
+si la letra fuera la "A", "A - A = 0", por lo que "word" que es 1 seria "1 + 0 = 1", asi que imprimiria "A" una sola vez.
+Y asi se imprime cada letra tantas veces como su posicion en el abecedario, que se a guardado en "word".
+Pasamos a la siguiente letra aumentando "c" para pasar a la siguiente letra si es que existe, y asi con todo el string.
 Ponemos un salto de linea y finalizamos programa.
 */
