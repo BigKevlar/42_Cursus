f561eff98a40e7549b6de9fe99809e837562a180
BigKevlar <kevlaroscuro@gmail.com>
2024-01-24 15:04:47 +0100

    alpha mirror

--- exam_Rank_2/level_1/alpha_mirror.c
+++ exam_Rank_2/level_1/alpha_mirror.c
@@ -32,7 +32,7 @@ $>
 
 int	main(int ac, char **av)
 {
-	int 	c = 0; //provamos a poner el contador solo con una letra, para simplificar codigo.
+	int 	c = 0;
 	char	*str = av[1]; //provamos a meter el argumento dentro de un string, para simpificar codigo. 
 
 	if (ac == 2)
@@ -50,3 +50,14 @@ int	main(int ac, char **av)
 	write(1, "\n", 1);
 	return (0);
 }
+
+/*
+Pseudocódigo:
+--------------------------------------------------------------------------------
+Nuestro programa tomará un string y cambiara sus letras por la opueta en el abecedario.
+Incluimos la libreria "unistd.h" para usar la funcion "write".
+El contador "c" lo usaremos para iterar entre las posiciones del argumento.
+Vamos a probar algo nuevo, para simplificar codigo. Vamos a meter todo el string 
+Si son exactamente dos argumentos entra en la condicion "if", si no pásala.
+
+*/
