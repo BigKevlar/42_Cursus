fcb71512468063eb0b6c9828f3cba76fc997a533
BigKevlar <kevlaroscuro@gmail.com>
2024-01-29 18:15:24 +0100

    libft and push_swap .c and .h

---
+++ push_swap/push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevlar <kevlar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 17:32:16 by kevlar            #+#    #+#             */
+/*   Updated: 2024/01/29 18:12:35 by kevlar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+/*
+Libreria 42 jmartos-
+*/
+# include "libft.h"
+
+
+
+/*
+Prototipo de funcione exclusivas para push_swap.
+*/
+
+
+#endif
