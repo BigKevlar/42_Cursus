ccde0364eeb1638cdd8fa3c916048849bb010cdc
BigKevlar <kevlaroscuro@gmail.com>
2024-01-30 20:26:33 +0100

    library

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,23 +6,34 @@
 /*   By: kevlar <kevlar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 17:32:16 by kevlar            #+#    #+#             */
-/*   Updated: 2024/01/29 18:12:35 by kevlar           ###   ########.fr       */
+/*   Updated: 2024/01/30 19:46:14 by kevlar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-/*
-Libreria 42 jmartos-
-*/
-# include "libft.h"
+# include "libft.h" /* Libreria 42 jmartos- */
+# include <stdio.h> /* printf */
+# include <stddef.h> /* size_t */
+# include <stdlib.h> /* atoi, malloc, free */
+# include <limits.h> /* ¿? */
 
+typedef struct s_stack
+{
+	int				value;
+	int				pos;
+	int				action;
+	struct s_stack	*next;
+}					t_stack;
 
-
-/*
-Prototipo de funcione exclusivas para push_swap.
-*/
-
+/* Prototipo de funcione exclusivas para push_swap. */
+t_stack	*create_stack(int value)
+funcion (check_stack)
+funcion (sort_stack)
+funcion (do_swap)
+funcion (do_push)
+funcion (do_rotate)
+funcion (do_reverse_rotate)
 
 #endif
