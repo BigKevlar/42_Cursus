aca1cdec41d270f4b8482657f494e7d60b960019
BigKevlar <kevlaroscuro@gmail.com>
2024-02-05 20:16:53 +0100

    alpha_mirror

--- exam_Rank_2/level_1/alpha_mirror.c
+++ exam_Rank_2/level_1/alpha_mirror.c
@@ -33,16 +33,16 @@ $>
 int	main(int ac, char **av)
 {
 	int 	c = 0;
-	char	*str = av[1]; //provamos a meter el argumento dentro de un string, para simpificar codigo. 
+	char	*str = av[1];
 
 	if (ac == 2)
 	{
 		while (str[c] != '\0')
 		{
-			if (str[c] >= 'a' && str[c] <= 'z') //si es minuscula le restamos al caracter ascii z el valor y sumamos a.
-				str[c] = ('z' - str[c]) + 'a';
-			else if (str[c] >= 'A' && str[c] <= 'Z') //si es minuscula hacemos lo mismo pero con letras mayusculas.
+			if (str[c] >= 'A' && str[c] <= 'Z')
 				str[c] = ('Z' - str[c]) + 'A';
+			else if (str[c] >= 'a' && str[c] <= 'z')
+				str[c] = ('z' - str[c]) + 'a';
 			write(1, &str[c], 1);
 			c++;
 		}
@@ -57,7 +57,14 @@ Pseudocódigo:
 Nuestro programa tomará un string y cambiara sus letras por la opueta en el abecedario.
 Incluimos la libreria "unistd.h" para usar la funcion "write".
 El contador "c" lo usaremos para iterar entre las posiciones del argumento.
-Vamos a probar algo nuevo, para simplificar codigo. Vamos a meter todo el string 
+Vamos a probar algo nuevo para simplificar codigo. Vamos a meter todo 2º argumento en un string. 
 Si son exactamente dos argumentos entra en la condicion "if", si no pásala.
-
+Mientras dure el argumento que le hemos pasado:
+Si esta entre 'A' y 'Z' tenemos que al valor 'Z' en ASCII restarle el de la letra pasada y sumarle el de 'A'.
+Para las minusculas igual pero con minusculas.
+Por ejemplo, pongamos que es la letra H. Como tomas los valores ASCII quedaria algo asi:
+H = (Z - H) + A <------> 72 = (90 - 72) + 65
+Se sustituiria por la S, que es la 8A letra del abecedario empezando por abajo, homologo a la H.
+Al final escribimos la letra y aumentamos el contador para pasar a la siguiente letra.
+Al teminar el bucle, o si no ha entrado en el bucle, imprimimos un salto de linea y fin del programa.
 */
