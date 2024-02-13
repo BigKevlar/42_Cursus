b166e4383a91f9283c245d43b1726bcc21b1eb35
BigKevlar <kevlaroscuro@gmail.com>
2024-02-13 17:11:57 +0100

    ft_strcmp

--- exam_Rank_2/level_1/ft_strcmp.c
+++ exam_Rank_2/level_1/ft_strcmp.c
@@ -11,7 +11,7 @@ Your function must be declared as follows:
 int    ft_strcmp(char *s1, char *s2);
 */
 
-int	ft_strcmp(char *s1, char *s2) //compara un string con otro.
+int	ft_strcmp(char *s1, char *s2)
 {
 	int	c = 0;
 
@@ -23,6 +23,12 @@ int	ft_strcmp(char *s1, char *s2) //compara un string con otro.
 /*
 Pseudocódigo:
 --------------------------------------------------------------------------------
-Nuestra funcion compara ambos string en orden lexicografico.
-
+Nuestra funcion compara ambos string en orden lexicografico (orden numerico en el abecedario).
+La funcion es sencilla. Iniciamos una variable para iterar las posiciones de los dos strings.
+Luego hacemos un bucle: mientras no lleguen ningunos de los dos al caracter nulo y sean
+iguales sus caracteres sigue comparando.
+Una vez salgamos del bucle restamos ambos y finalizamos funcion. El resultado se interpreta asi:
+- Si el resultado es = 0, los dos strings son iguales.
+- Si el resultado es > 0, el primer string es mayor.
+- si el resultado es < 0, el segundo string es mayor.
 */
