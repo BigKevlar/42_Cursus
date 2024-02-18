1e4e4104a2cb0e54ce76159a5b53e8523ef18c94
BigKevlar <kevlaroscuro@gmail.com>
2024-02-18 21:02:04 +0100

    keep going...

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,18 +6,55 @@
 /*   By: kevlar <kevlar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 17:31:30 by kevlar            #+#    #+#             */
-/*   Updated: 2024/02/13 17:07:06 by kevlar           ###   ########.fr       */
+/*   Updated: 2024/02/18 20:35:55 by kevlar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void ft_isnumber(char *numbers)
+{
+	int	c;
+
+	c = 0;
+	while (numbers[c] != '\0')
+	{
+		if (numbers[c] <)		
+	}
+	return ;
+}
+
+int	main(int ac, char **av)
+{
+	t_stack	*a;
+	t_stack *b;
+	int		c;
+
+	a = NULL;
+	b = NULL;
+	c = 0;
+	if ((ac == 1) || (ac == 2 && !av[1][0]))
+	{
+		ft_printf("%s\n", "ERROR, argumentos vacios o inexistentes!");
+		return (1);
+	}
+	else if (ac == 2)
+	{
+		av = split(av[1], ' ');
+		if (ft_isnumber(av[1]))
+			return (0);
+		
+	}
+	return (0);
+}
+
+/*
 int	main(int ac, char **av)
 {
 	(void)ac;
 	t_int *num1;
 	t_int *num2;
-	t_stack *all;
+	t_stack_test *all;
 	
 	num1 = malloc(1 * sizeof(t_int));
 	all = malloc(1 * sizeof(t_list));
@@ -26,4 +63,4 @@ int	main(int ac, char **av)
 	num2 = all->a->content;
 	printf("%ld\n", num2->value);
 }
-
+*/
--- push_swap/swap.c
+++ push_swap/swap.c
@@ -1,40 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kevlar <kevlar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/24 17:32:16 by kevlar            #+#    #+#             */
-/*   Updated: 2024/02/18 20:08:55 by kevlar           ###   ########.fr       */
+/*   Created: 2024/02/15 13:07:12 by kevlar            #+#    #+#             */
+/*   Updated: 2024/02/15 13:07:13 by kevlar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include "libft/includes/libft.h" /* Libreria 42 jmartos- */
-# include <stdlib.h> /* malloc */
-
-typedef struct s_stack
-{
-	int				num;
-	struct s_stack	*prev;
-	struct s_stack	*next;
-} t_stack;
-
-/*
-typedef struct s_stack_test
-{
-	char				**str;
-	t_list *a;
-	t_list *b;
-} t_stack_test;
-
-typedef struct s_int
-{
-	long value;
-	
-} t_int;
-*/
-#endif
