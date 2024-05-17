6aa2e2ad51fb809827ee1b2c52a8dedec24759fa
Juan Carlos Martos Vergara <jmartos-@c1r10s1.42malaga.com>
2024-05-17 16:21:50 +0200

    max

--- exam_Rank_2/level_1/max.c
+++ exam_Rank_2/level_1/max.c
@@ -1,32 +1,14 @@
-/*
-Assignment name  : max
-Expected files   : max.c
-Allowed functions:
---------------------------------------------------------------------------------
-
-Write the following function:
-
-int		max(int* tab, unsigned int len);
-
-The first parameter is an array of int, the second is the number of elements in
-the array.
-
-The function returns the largest number found in the array.
-
-If the array is empty, the function returns 0.
-*/
-
 int	max(int *tab, unsigned int len)
 {
-	int aux = tab[0];
+	int max = tab[0];
 
 	if (len == 0)
 		return (0);
 	while (len > 0)
 	{
-		if (aux < tab[len - 1])
-			aux = tab[len - 1];
+		if (max < tab[len - 1])
+			max = tab[len - 1];
 		len--;
 	}
-	return (aux);
+	return (max);
 }
