8ba98c663a05c14dcbb4ec9ffe1239f2c27bf2a2
Juan Carlos Martos Vergara <jmartos-@c1r10s1.42malaga.com>
2024-05-17 16:20:08 +0200

    first_word

--- exam_Rank_2/level_0/first_word.c
+++ exam_Rank_2/level_0/first_word.c
@@ -1,33 +1,3 @@
-/*
-Assignment name  : first_word
-Expected files   : first_word.c
-Allowed functions: write
---------------------------------------------------------------------------------
-
-Write a program that takes a string and displays its first word, followed by a
-newline.
-
-A word is a section of string delimited by spaces/tabs or by the start/end of
-the string.
-
-If the number of parameters is not 1, or if there are no words, simply display
-a newline.
-
-Examples:
-
-$> ./first_word "FOR PONY" | cat -e
-FOR$
-$> ./first_word "this        ...       is sparta, then again, maybe    not" | cat -e
-this$
-$> ./first_word "   " | cat -e
-$
-$> ./first_word "a" "b" | cat -e
-$
-$> ./first_word "  lorem,ipsum  " | cat -e
-lorem,ipsum$
-$>
-*/
-
 #include <unistd.h>
 
 int main(int ac, char **av)
@@ -38,7 +8,7 @@ int main(int ac, char **av)
 	{
 		while (av[1][c] == ' ' || av[1][c] == '\t')
 			c++;
-		while (av[1][c] != ' ' && av[1][c] != '\t' && av[1][c] != '\0')
+		while (av[1][c] != '\0' && av[1][c] != ' ' && av[1][c] != '\t')
 		{
 			write (1, &av[1][c], 1);
 			c++;
@@ -58,7 +28,7 @@ Pseudocódigo:
 	El contador "c" lo usaremos para iterar entre las posiciones del argumento.
 	Si son exactamente dos argumentos entra en la condicion "if", si no pásala.
 	Saltamos los espacios o tabulaciones que pudieran haber antes de la primera palabra.
-	Escribe mientras no sea ni espacio, ni tabulacion, ni caracter nulo.
+	Escribe mientras no sea ni caracter nulo, ni espacio, ni tabulacion.
 	Si no son exactamente dos caracteres, o si finaliza de escribir, escribe un salto de linea.
 	Finalizamos el programa.
 --------------------------------------------------------------------------------
