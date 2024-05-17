71eb1281805e9493c81a36c183adbc6b2fea21e0
Juan Carlos Martos Vergara <jmartos-@c1r10s1.42malaga.com>
2024-05-17 16:22:15 +0200

    snake_to_camel

--- exam_Rank_2/level_1/snake_to_camel.c
+++ exam_Rank_2/level_1/snake_to_camel.c
@@ -1,27 +1,3 @@
-/*
-Assignment name  : snake_to_camel
-Expected files   : snake_to_camel.c
-Allowed functions: malloc, free, realloc, write
---------------------------------------------------------------------------------
-
-Write a program that takes a single string in snake_case format
-and converts it into a string in lowerCamelCase format.
-
-A snake_case string is a string where each word is in lower case, separated by
-an underscore "_".
-
-A lowerCamelCase string is a string where each word begins with a capital letter
-except for the first one.
-
-Examples:
-$>./snake_to_camel "here_is_a_snake_case_word"
-hereIsASnakeCaseWord
-$>./snake_to_camel "hello_world" | cat -e
-helloWorld$
-$>./snake_to_camel | cat -e
-$
-*/
-
 #include <unistd.h>
 
 int	main(int ac, char **av)
@@ -36,11 +12,11 @@ int	main(int ac, char **av)
 		{
 			if (av[1][c] == '_')
 			{
-				c++; //saltamos posicion del "_".
-				if (av[1][c] >= 'a' && av[1][c] <= 'z') //verificamos si son minusculas y...
-					av[1][c] -= 32; //... cambiamos a mayusculas.
+				c++;
+				if (av[1][c] >= 'a' && av[1][c] <= 'z')
+					av[1][c] -= 32;
 			}
-			write(1, &av[1][c], 1); //tanto si lo es como si no imprimimos y saltamos de posicion.
+			write(1, &av[1][c], 1);
 			c++;
 		}
 	}
