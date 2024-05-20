feec7e2ded96a4afd8a4f8fc9447b8511541ba4f
BigKevlar <kevlaroscuro@gmail.com>
2024-05-20 20:48:28 +0200

    FT_strcpy

--- exam_Rank_2/level_0/ft_strcpy.c
+++ exam_Rank_2/level_0/ft_strcpy.c
@@ -1,16 +1,3 @@
-/*
-Assignment name  : ft_strcpy
-Expected files   : ft_strcpy.c
-Allowed functions: 
---------------------------------------------------------------------------------
-
-Reproduce the behavior of the function strcpy (man strcpy).
-
-Your function must be declared as follows:
-
-char    *ft_strcpy(char *s1, char *s2);
-*/
-
 char	*ft_strcpy(char *s1, char *s2)
 {
 	int c = 0;
