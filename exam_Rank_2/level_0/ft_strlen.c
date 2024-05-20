fd332ddc3915600f4396db285beaa9d031b41168
BigKevlar <kevlaroscuro@gmail.com>
2024-05-20 20:48:41 +0200

    FT_strlen

--- exam_Rank_2/level_0/ft_strlen.c
+++ exam_Rank_2/level_0/ft_strlen.c
@@ -1,16 +1,3 @@
-/*
-Assignment name  : ft_strlen
-Expected files   : ft_strlen.c
-Allowed functions:
---------------------------------------------------------------------------------
-
-Write a function that returns the length of a string.
-
-Your function must be declared as follows:
-
-int	ft_strlen(char *str);
-*/
-
 int	ft_strlen(char *str)
 {
 	int c = 0;
