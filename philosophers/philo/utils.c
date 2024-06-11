154c23f55e2eb48f2d0f511f9c83b5c96184398b
jmartos- <jcmv.42malaga@gmail.com>
2024-06-11 23:57:38 +0200

    Agregando funciones

--- philosophers/philo/utils.c
+++ philosophers/philo/utils.c
@@ -6,35 +6,40 @@
 /*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 18:15:02 by jmartos-          #+#    #+#             */
-/*   Updated: 2024/04/29 18:46:48 by jmartos-         ###   ########.fr       */
+/*   Updated: 2024/06/11 23:49:54 by jmartos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	ft_atol(char *str, t_stack *s)
+int	atol(char *str)
 {
 	int		c;
 	long	num;
-	int		sign;
+	int		sig;
 
 	c = 0;
 	num = 0;
-	sign = 1;
+	sig = 1;
 	while (str[c] == ' ' || (str[c] >= '\t' && str[c] <= '\r'))
 		c++;
 	if (str[c] == '+' || str[c] == '-')
 	{
 		if (str[c] == '-')
-			sign *= -1;
+			sig = -1;
 		c++;
 	}
 	while (str[c] >= '0' && str[c] <= '9' && str[c] != '\0')
 	{
 		num = num * 10 + (str[c] - '0');
-		if (num > INT_MAX || (num * sign) < INT_MIN)
-			error_and_free(s, "Error");
 		c++;
 	}
-	return ((int)num * sign);
+	return ((int)num * sig);
+}
+
+int	check_int(long num)
+{
+	if (num > INT_MAX || num < INT_MIN)
+		return (1)
+	return (0);
 }
