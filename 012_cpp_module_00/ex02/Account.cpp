334c62904a9df5a101e8550b2b09cb86cde7345a
jmartos- <jcmv.42malaga@gmail.com>
2024-08-06 20:06:30 +0200

    ex02 in progress

--- 012_cpp_module_00/ex02/Account.cpp
+++ 012_cpp_module_00/ex02/Account.cpp
@@ -18,3 +18,68 @@
     clang: error: linker command failed with exit code 1 (use -v to see invocation)
 */
 
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <sstream>
+#include <Account.hpp>
+
+Account::Account(int initial_deposit)
+{
+    this->_nbAccounts = 0;
+	this->_totalAmount = 0;
+	this->_totalNbDeposits = 0;
+	this->_totalNbWithdrawals = 0;
+
+    this->_accountIndex = 0;
+    this->_amount = 0;
+    this->_nbDeposits = 0;
+    this->_nbWithdrawals = 0;
+};
+
+Account::~Account(){};
+
+int Account::getNbAccounts()
+{
+    return(_nbAccounts);
+}
+
+int Account::getTotalAmount()
+{
+    return(_totalAmount);
+}
+
+int Account::getNbDeposits()
+{
+    return(_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals()
+{
+    return(_totalNbWithdrawals);
+}
+
+/* [19920104_091532] */
+void	_displayTimestamp()
+{
+        std::time_t now;
+
+        now = std::time(NULL);
+        std::cout << "[" << std::put_time(localtime(&now), "%Y%m%d_%H%M%S ") << "]";
+}
+
+/* [19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0 */
+void    Account::displayAccountsInfos()
+{
+    Account::_displayTimestamp();
+    std::cout << "accounts:" << getNbAccounts() << ";";
+    std::cout << "total:" << getTotalAmount() << ";";
+    std::cout << "deposits;" << getNbDeposits() << ";";
+    std::cout << "withdrawals:" << getNbWithdrawals() << ";" << std::endl; 
+}
+
+void	makeDeposit( int deposit );
+bool	makeWithdrawal( int withdrawal );
+int		checkAmount( void ) const;
+void	displayStatus( void ) const;
+Account( void );
--- 012_cpp_module_00/ex02/tests.cpp
+++ 012_cpp_module_00/ex02/tests.cpp
@@ -25,17 +25,17 @@ int		main( void ) {
 	accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
 
-	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
-	size_t const		d_size( sizeof(d) / sizeof(int) );
-	ints_t				deposits( d, d + d_size );
-	ints_t::iterator	dep_begin	= deposits.begin();
-	ints_t::iterator	dep_end		= deposits.end();
-
-	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
-	size_t const		w_size( sizeof(w) / sizeof(int) );
-	ints_t				withdrawals( w, w + w_size );
-	ints_t::iterator	wit_begin	= withdrawals.begin();
-	ints_t::iterator	wit_end		= withdrawals.end();
+	int	const				d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
+	size_t const			d_size( sizeof(d) / sizeof(int) );
+	ints_t					deposits( d, d + d_size );
+	ints_t::iterator		dep_begin	= deposits.begin();
+	ints_t::iterator		dep_end		= deposits.end();
+	
+	int	const				w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
+	size_t const			w_size( sizeof(w) / sizeof(int) );
+	ints_t					withdrawals( w, w + w_size );
+	ints_t::iterator		wit_begin	= withdrawals.begin();
+	ints_t::iterator		wit_end		= withdrawals.end();
 
 	Account::displayAccountsInfos();
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
