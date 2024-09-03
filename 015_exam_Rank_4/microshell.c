0657c63ecf0873988b4b98a6a47ed617c07d8743
jmartos- <jcmv.42malaga@gmail.com>
2024-09-03 21:17:24 +0200

    exam pass grademe

--- 015_exam_Rank_4/microshell.c
+++ 015_exam_Rank_4/microshell.c
@@ -43,12 +43,12 @@ void err(char *str)
 		write(2, str++, 1);
 }
 
-int cd(char **argv, int i)
+int cd(char **av, int i)
 {
 	if (i != 2)
 		return err("error: cd: bad arguments\n"), 1;
-	if (chdir(argv[1]) == -1)
-		return err("error: cd: cannot change directory to "), err(argv[1]), err("\n"), 1;
+	if (chdir(av[1]) == -1)
+		return err("error: cd: cannot change directory to "), err(av[1]), err("\n"), 1;
 	return 0;
 }
 
@@ -58,14 +58,14 @@ void set_pipe(int has_pipe, int *fd, int end)
 		err("error: fatal\n"), exit(1);
 }
 
-int	exec(char **argv, int i, char **envp)
+int	exec(char **av, int i, char **env)
 {
 	int has_pipe, fd[2], pid, status;
 
-	has_pipe = argv[i] && !strcmp(argv[i], "|");
+	has_pipe = av[i] && !strcmp(av[i], "|");
 
-	if (!has_pipe && !strcmp(*argv, "cd"))
-		return cd(argv, i);
+	if (!has_pipe && !strcmp(*av, "cd"))
+		return cd(av, i);
 
 	if (has_pipe && pipe(fd) == -1)
 		err("error: fatal\n"), exit(1);
@@ -74,30 +74,31 @@ int	exec(char **argv, int i, char **envp)
 		err("error: fatal\n"), exit(1);
 	if (!pid)
 	{
-		argv[i] = 0;
+		av[i] = 0;
 		set_pipe(has_pipe, fd, 1);
-		if (!strcmp(*argv, "cd"))
-			exit(cd(argv, i));
-		execve(*argv, argv, envp);
-		err("error: cannot execute "), err(*argv), err("\n"), exit(1);
+		if (!strcmp(*av, "cd"))
+			exit(cd(av, i));
+		execve(*av, av, env);
+		err("error: cannot execute "), err(*av), err("\n"), exit(1);
 	}
 	waitpid(pid, &status, 0);
 	set_pipe(has_pipe, fd, 0);
 	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
-int main(int, char **argv, char **envp)
+int main(int ac, char **av, char **env)
 {
+	(void)ac;
 	int i = 0, status = 0;
 
-	while (argv[i])
+	while (av[i])
 	{
-		argv += i + 1;
+		av += i + 1;
 		i = 0;
-		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
 			i++;
 		if (i)
-			status = exec(argv, i, envp);
+			status = exec(av, i, env);
 	}
 	return status;
 }
