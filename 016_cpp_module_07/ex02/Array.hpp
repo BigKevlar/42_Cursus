ff2823cf4469fe216dbdafeb9bebd2951df48aa0
jmartos- <jcmv.42malaga@gmail.com>
2024-10-03 11:52:09 +0200

    all ready

--- 016_cpp_module_07/ex02/Array.hpp
+++ 016_cpp_module_07/ex02/Array.hpp
@@ -6,82 +6,82 @@
 /*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 20:54:44 by jmartos-          #+#    #+#             */
-/*   Updated: 2024/10/02 23:16:11 by jmartos-         ###   ########.fr       */
+/*   Updated: 2024/10/03 11:47:39 by jmartos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <cstdlib>
 
 template <typename T>
 class Array
 {
 	private:
-		T	*_array;
-		T	_len;
+		T				*_array;
+		unsigned int	_len;
 	public:
-		Array <T>()
+		Array <T> ()
 		{
 			std::cout << "[ Default constructor called! ]" << std::endl;
-			_array = NULL;
 			_len = 0;
-		}
-		
-		Array <T>(unsigned int n)
-		{
-			std::cout << "[ Constructor called! ]" << std::endl;
-			_array = new T*[n];
-			_len = n;
-			for (int i = 0; i < n; i++)
-				_array[i] = new T();
+			_array = NULL;
 		}
 
-		Array <T>(const Array <T> &copy_)
+		~Array <T> ()
+		{
+			if (_array != NULL)
+				delete [] _array;
+		}
+		
+		Array <T> (unsigned int n)
+		{
+			std::cout << "[ Constructor called! ]" << std::endl;
+			_len = n;
+			_array = new T[_len];
+		}
+
+		Array <T> (const Array <T> &copy_)
 		{
 			std::cout << "[ Copy constructor called! ]" << std::endl;
-			for (unsigned int i = 0; i < _len; i++)
-				delete _array[i];
-			delete[] _array;
-			_array = new T*[copy_._len];
 			_len = copy_._len;
-			for (int i = 0; i < n; i++)
-				_array[i] = new T();
+			_array = new T[copy_._len];
+			for (unsigned int i = 0; i < _len; i++)
+				_array[i] = copy_._array[i];
 			*this = copy_;
 		}
 		
 		Array <T> &operator=(const Array <T> &array_)
 		{
+			if (_array != NULL)
+				delete [] _array;
+			_array = new T[array_._len];
+			_len = array_._len;
 			for (unsigned int i = 0; i < _len; i++)
-				delete _array[i];
-			delete[] _array;
-			_array = new T*[array_._len];
-			_lenght = array_._len;
-			for (unsigned int i = 0; i < _lenght; i++)
-				_array[i] = new T(*array_._array[i]);
-
+				_array[i] = array_._array[i];
 			return (*this);
 		}
 
-		T	&operator[](size_t i)
+		T &operator[](size_t i)
 		{
-			if (i >= _lenght)
-				throw IndexOutOfBounds();
-			return (*_array[i]);
+			if (i >= _len)
+				throw IndexIsOutOfBounds();
+			return (_array[i]);
 		}
 
-		size_t	size(void) const
-		{
-			return (_len);
-		}
-
-		class IndexOutOfBounds: public std::exception
+		class IndexIsOutOfBounds: public std::exception
 		{
 			public:
-				const	*what() const throw()
+				virtual char const	*what() const throw()
 				{
 					return ("Index out of bounds");
 				}
 
 		};
+
+		size_t	size(void) const
+		{
+			return (_len);
+		}
 };
